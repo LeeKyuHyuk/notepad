691469305438bcd01c3d051dac9b194c4694ae8f
KyuHyuk Lee <lee@kyuhyuk.kr>
2021-12-26 03:48:33 +0900

    Create Spring Book Backend Project

---
+++ backend/src/main/kotlin/kr/kyuhyuk/notepad/NotepadApplication.kt
@@ -0,0 +1,11 @@
+package kr.kyuhyuk.notepad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotepadApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotepadApplication>(*args)
+}
