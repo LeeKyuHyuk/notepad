8466a22627b453fcf1b310ba2046381d1ae026e2
KyuHyuk Lee <lee@kyuhyuk.kr>
2021-12-26 04:13:22 +0900

    Create Notepad Entity

--- backend/src/main/kotlin/kr/kyuhyuk/notepad/NotepadApplication.kt
+++ backend/src/main/kotlin/kr/kyuhyuk/notepad/NotepadApplication.kt
@@ -2,10 +2,12 @@ package kr.kyuhyuk.notepad
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class NotepadApplication
 
 fun main(args: Array<String>) {
-	runApplication<NotepadApplication>(*args)
+    runApplication<NotepadApplication>(*args)
 }
